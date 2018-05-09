007c24c22cccbed7e3fd961449a933ec8a867969
Your Name <you@example.com>
2018-05-09 20:07:25 +0530

    Modified api url

--- src/cljs/myra/settings.cljs
+++ src/cljs/myra/settings.cljs
@@ -3,9 +3,9 @@
 (def settings
   {:jwt-name     "myra-jwt"
    :gql-endpoint {:dev  "http://localhost:4000/api/graphql"
-                  :prod "https://myra-backend-staging.herokuapp.com/api/graphql"}})
+                  :prod "https://tranquil-falls-15561.herokuapp.com/api/graphql"}})
 
 (def websocket-endpoint
   (if js/goog.DEBUG
     "ws://localhost:4000/socket"
-    "wss://myra-backend-staging.herokuapp.com/socket"))
+    "wss://tranquil-falls-15561.herokuapp.com/socket"))
